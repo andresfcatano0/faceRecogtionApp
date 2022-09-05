7f5209601b466dcbfcbd3da1e093df71b2707cb7
Andres Catano <andresfcatano0@gmail.com>
2022-09-04 20:45:17 -0400

    Add FaceRecognition component

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FaceRecognition = () => {
+  return (
+    <div className="center">
+      <img src="https://www.themoviedb.org/t/p/w500/xndWFsBlClOJFRdhSt4NBwiPq2o.jpg" />
+    </div>
+  );
+}
+
+export default FaceRecognition;
