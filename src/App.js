f7fea9194ef0cf64322828fc6571a73eb2b37d2c
Andres Catano <andresfcatano0@gmail.com>
2022-09-24 10:01:18 -0400

    Fix typo

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ class App extends React.Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input})
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://secure-harbor-97097.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'content-type': 'application/json'},
         body: JSON.stringify({
@@ -72,7 +72,7 @@ class App extends React.Component {
       .then(response => response.json())
       .then(response => {
         if (response){
-          fetch('http://localhost:3000/image', {
+          fetch('https://secure-harbor-97097.herokuapp.com/image', {
             method: 'put',
             headers: {'content-type': 'application/json'},
             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://secure-harbor-97097.herokuapp.com/register', {
       method: 'post',
       headers: {'content-type': 'application/json'},
       body: JSON.stringify({
